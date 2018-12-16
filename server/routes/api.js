0dde7d10f9538f8d75e1b83b3c104f0fbda25244
gabtoschi <gabriel.toschi@hotmail.com>
2018-12-16 16:25:16 -0200

    Connect Mongoose to server file

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,10 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
+
+const dbHost = 'mongodb://database/myevents';
+mongoose.connect(dbHost);
+console.log(mongoose.connection.readyState);
 
 router.get('/', (req, res) => {
     res.send('api works');
