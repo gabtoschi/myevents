f3bb76aaadd7f4364e8bcb192bd9029ad7a55182
Gabriel Toschi <gabriel.toschi@hotmail.com>
2018-12-16 19:05:08 -0200

    Create interface for Registerform component

--- client/src/app/registerform/registerform.component.ts
+++ client/src/app/registerform/registerform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-registerform',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registerform.component.css']
 })
 export class RegisterformComponent implements OnInit {
+
+  registerForm = new FormGroup({
+    name: new FormControl(),
+    email: new FormControl(),
+    password: new FormControl()
+  });
+
+  onSubmit() {
+    console.warn(this.registerForm.value);
+  }
 
   constructor() { }
 
