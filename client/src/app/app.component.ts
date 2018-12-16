436897b2fa80ee6ddf3931ced7f8f4defd84e1da
gabtoschi <gabriel.toschi@hotmail.com>
2018-12-16 18:29:12 -0200

    Make examples to connects all the parts of the system (front + back + DB)

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+
+  APIUrl = 'http://localhost:3000';
+
+  // Declare empty list of people
+  people: Object[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  // Angular 2 Life Cycle event when component has been initialized
+  ngOnInit() {
+    this.addPerson('teste4', '44');
+    this.getAllPeople();
+  }
+
+  // Add one person to the API
+  addPerson(name, age) {
+    this.http.post(this.APIUrl + '/users', {'name': name, 'age': age})
+      .subscribe(
+        (response) => { console.log('response: ' + response) },
+        (error) => { console.log('error: ' + error)}
+      )
+  }
+
+  getAllPeople(){
+    this.http.get(this.APIUrl + '/users')
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+
+  // Get all users from the API
+  /*getAllPeople() {
+    this.http.get(`${this.API}/users`)
+      .map(res => res.json())
+      .subscribe(people => {
+        console.log(people)
+        this.people = people
+      })
+  }*/
 }
--- server/routes/api.js
+++ server/routes/api.js
@@ -6,6 +6,35 @@ const dbHost = 'mongodb://database/myevents';
 mongoose.connect(dbHost);
 console.log(mongoose.connection.readyState);
 
+const userSchema = new mongoose.Schema({
+    name: String,
+    age: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+router.get('/users', (req, res) => {
+    User.find(function (err, result){
+        if (err) res.send(err);
+        res.json(result);        
+    });
+});
+
+router.post('/users', (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        age: req.body.age
+    });
+
+    user.save(error => {
+        if (error) res.status(500).send(error);
+
+        res.status(201).json({
+            message: 'User created successfully.'
+        });
+    });
+});
+
 router.get('/', (req, res) => {
     res.send('api works');
 });
